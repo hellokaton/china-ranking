f32d85f036e064c1e2e9b3e46aae84b76302892e
hellokaton <hellokaton@gmail.com>
2025-11-29 20:37:11 +0800

    更新站点配置，将域名更改为新的地址

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   // site: "http://localhost:4321",
-  site: "https://china-ranking.vercel.app",
+  site: "https://china-ranking.32k.site",
   integrations: [tailwind()],
 });
